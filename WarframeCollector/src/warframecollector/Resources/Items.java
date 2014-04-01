bc56b9af3cf0e5bc0969bf5b92f0d7d14b2ec35e
Joseph Sessions <Jtsessions@crimson.ua.edu>
2014-04-01 18:33:34 -0500

    Adds Items.java for managing static lists of items.

---
+++ WarframeCollector/src/warframecollector/Resources/Items.java
@@ -0,0 +1,213 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package warframecollector.Resources;
+
+/**
+ *
+ * @author Joseph
+ */
+public final class Items {
+    
+    public static final String[] warframes = {
+        "Ash",
+        "Banshee",
+        "Ember",
+        "Ember Prime",
+        "Excalibur",
+        "Excalibur Prime",
+        "Frost",
+        "Frost Prime",
+        "Loki",
+        "Mag",
+        "Mag Prime",
+        "Nekros",
+        "Nova",
+        "Nyx",
+        "Oberon",
+        "Rhino",
+        "Rhino Prime",
+        "Saryn",
+        "Trinity",
+        "Valkyr",
+        "Vauban",
+        "Volt",
+        "Zephyr"
+    };
+    
+    public static final String[] primaries = {
+        "Boar",
+        "Boar Prime",
+        "Boltor",
+        "Boltor Prime",
+        "Braton",
+        "Braton Prime",
+        "Braton Vandal",
+        "Burston",
+        "Burston Prime",
+        "Cernos",
+        "Dera",
+        "Drakgoon",
+        "Dread",
+        "Flux Rifle",
+        "Gorgon",
+        "Gorgon Wraith",
+        "Grakata",
+        "Grinlok",
+        "Hek",
+        "Hind",
+        "Ignis",
+        "Karak",
+        "Lanka",
+        "Latron",
+        "Latron Prime",
+        "Miter",
+        "MK1-Braton",
+        "Ogris",
+        "Paris",
+        "Paris Prime",
+        "Penta",
+        "Phage",
+        "Snipetron",
+        "Snipetron Vandal",
+        "Sobek",
+        "Soma",
+        "Strun",
+        "Strun Wraith",
+        "Supra",
+        "Synapse",
+        "Tetra",
+        "Tigris",
+        "Torid",
+        "Vectis",
+        "Vulkar"
+    };
+    
+    public static final String[] secondaries = {
+        "Acrid",
+        "Akbolto",
+        "Akbronco",
+        "Akbronco Prime",
+        "Aklato",
+        "Aklex",
+        "Akmagnus",
+        "Akstilleto",
+        "Akvasto",
+        "Ballistica",
+        "Bolto",
+        "Brakk",
+        "Bronco",
+        "Bronco Prime",
+        "Castanas",
+        "Cestra",
+        "Despair",
+        "Detron",
+        "Dex Afuris",
+        "Dual Afuris",
+        "Dual Cestra",
+        "Embolist",
+        "Furis",
+        "Hikou",
+        "Kraken",
+        "Kunai",
+        "Lato",
+        "Lato Prime",
+        "Lato Vandal",
+        "Lex",
+        "Magnus",
+        "Marelok",
+        "Seer",
+        "Sicarus",
+        "Sicarus Prime",
+        "Spectra",
+        "Stug",
+        "Twin Gremlins",
+        "Twin Vipers",
+        "Tysis",
+        "Vasto",
+        "Viper",
+        "Wraith Twin Vipers"
+    };
+    
+    public static final String[] melees = {
+        "Amphis",
+        "Ankyros",
+        "Ankyros Prime",
+        "Bo",
+        "Ceramic Dagger",
+        "Cronus",
+        "Dakra Prime",
+        "Dark Dagger",
+        "Dark Sword",
+        "Dual Cleavers",
+        "Dual Ether",
+        "Dual Heat Swords",
+        "Dual Ichor",
+        "Dual Kamas",
+        "Dual Skana",
+        "Dual Zoren",
+        "Ether Daggers",
+        "Ether Reaper",
+        "Ether Sword",
+        "Fang",
+        "Fang Prime",
+        "Fragor",
+        "Furax",
+        "Galatine",
+        "Glaive",
+        "Glaive Prime",
+        "Gram",
+        "Hate",
+        "Heat Dagger",
+        "Heat Sword",
+        "Jat Kittag",
+        "Jaw Sword",
+        "Kama",
+        "Kestrel",
+        "Kogake",
+        "Lecta",
+        "Machete",
+        "Machete Wraith",
+        "Magistar",
+        "Mire",
+        "Obex",
+        "Orthos",
+        "Orthos Prime",
+        "Pangolin Sword",
+        "Plasma Sword",
+        "Prova",
+        "Prova Vandal",
+        "Reaper Prime",
+        "Scindo",
+        "Scoliac",
+        "Skana",
+        "Skana Prime",
+    };
+    
+    public static final String[] sentinels = {
+        "Carrier",
+        "Dethcube",
+        "Djinn",
+        "Helios",
+        "Shade",
+        "Wyrm"
+    };
+    
+    public static final String[] sentinelWeapons = {
+        "Burst Laser",
+        "Deconstructor",
+        "Deth Machine Rifle",
+        "Laser Rifle",
+        "Stinger",
+        "Sweeper"
+    };
+    
+    public static final String[] cosmetics = {
+        "PLACEHOLDER",
+        "ADDITIONAL PLACEHOLDER"
+    };
+    
+}
--- WarframeCollector/src/warframecollector/WarframeCollectorController.java
+++ WarframeCollector/src/warframecollector/WarframeCollectorController.java
@@ -6,7 +6,14 @@
 
 package warframecollector;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.sql.*;
+import warframecollector.Resources.Items;
+
 
 /**
  *
@@ -16,7 +23,13 @@ public class WarframeCollectorController {
     
     private WarframeCollectorView view;
     
-    public WarframeCollectorController( WarframeCollectorView viewToAdd ) {
+    public WarframeCollectorController( WarframeCollectorView viewToAdd ) throws Exception {
+        
+        if ( !saveFileExists() ) {
+            createSaveFile();
+            initializeSaveFile();
+        }
+        
         view = viewToAdd;
         
         String[] arr = new String[50];
@@ -25,6 +38,62 @@ public class WarframeCollectorController {
         view.populateWarframes( arrayList1 );
         
         view.initializeTableListeners();
+    }
+    
+    private Boolean saveFileExists() {
+        Path file = Paths.get("data.wcf");
+        
+        return Files.exists(file);
+    }
+    
+    private void createSaveFile() {
+        Path file = Paths.get("data.wcf");
+        
+        try {
+            Files.createFile(file);
+        } catch ( IOException e ) {
+            System.err.format("createFile error: %s%n", e);
+        }
+        
+    }
+    
+    private void initializeSaveFile() throws Exception {
+        initializeWarframes();
+/*
+        initializePrimaries();
+        initializeSecondaries();
+        initializeMelees();
+        initializeSentinels();
+        initializeSentinelWeapons();
+        initializeCosmetics();
+*/
+    }
+    
+    private void initializeWarframes() throws Exception {
+        Connection c = null;
+        Class.forName("org.sqlite.JDBC");
+        c = DriverManager.getConnection("jdbc:sqlite:data.wdb");
+        Statement statement = c.createStatement();
+        statement.execute("create table Warframes("
+                + "name varchar(30),"
+                + "owned INT,"
+                + "rank varchar(10),"
+                + "reactor INT,"
+                + "forma varchar(10))");
+            
+        
+        String[] warframes = Items.warframes;
+        
+        for ( int i = 0; i < warframes.length; i++ ) {
+            PreparedStatement prep = c.prepareStatement("insert into Warframes values(?,0,'Unranked',0,'None');");
+            prep.setString(1, warframes[i]);
+            prep.execute();
+        }
+        
+        ResultSet result = statement.executeQuery("select * from Warframes");
+        while (result.next()) {
+            System.out.println(result.getString("name") + " " + result.getInt("owned"));
+        }
     }
     
 }
